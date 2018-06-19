21fee99705ed442aac6feda614dad09fb6c6d89c
Jackohhh <jackythejack11@gmail.com>
2018-06-20 11:07:26 +1200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
-  bot.user.setActivity(`The New Defenders | /help`);
+  bot.user.setActivity(`The New Defenders | !!help`, { type: 'WATCHING' });
 });
 
 bot.on("message", async message => {
@@ -11,7 +11,7 @@ bot.on("message", async message => {
   if (message.author.bot) return;
   if (message.channel.type === "dm") return;
 
-  let prefix = '/';
+  let prefix = '!';
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
