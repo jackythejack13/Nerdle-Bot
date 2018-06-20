f49e07c2b1ee1a80688ba677478a03a178422fee
Jackohhh <jackythejack11@gmail.com>
2018-06-21 01:27:41 +1200

    Update index.js

--- index.js
+++ index.js
@@ -6,15 +6,33 @@ client.on("ready", async () => {
   client.user.setActivity(`The New Defenders | !!help`, { type: 'WATCHING' });
 });
 
-client.on("message", async message => {
+// Constant Variables 
+const prefix = '/';
+const ownerID = '183527322236878850';
 
-  if (message.author.bot) return;
-  if (message.channel.type === "dm") return;
-
-  let prefix = '!!';
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
+client.on('message', message => {
+	
+	// Variables
+	let args = message.content.slice(prefix.length).trim().split(' ');
+	let cmd = args.shift().toLowerCase();
+	
+	// Return Statements
+	if (message.author.bot) return;
+	if (!message.content.startsWith(prefix)) return;
+	
+	// Command Handler
+	try {
+		
+		// Auto-Reload
+		delete require.cache[require.resolve(`./commands/${cmd}.js`)];
+		
+		let commandFile = require(`./commands/${cmd}.js`);
+		commandFile.run(client, message, args);
+		
+	} catch (e) {
+		console.log(e.stack);
+	}
+});
 
   if (cmd === `${prefix}ping`){
     message.channel.send("Pong!");
