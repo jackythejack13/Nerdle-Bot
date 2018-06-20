54f7e88ea083a5da14866ea1214dd81465471dd4
Jackohhh <jackythejack11@gmail.com>
2018-06-21 01:18:23 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const Discord = require("discord.js");
-const bot = new Discord.Client({disableEveryone: true});
+const client = new Discord.Client({disableEveryone: true});
 
-bot.on("ready", async () => {
-  console.log(`${bot.user.username} is online!`);
-  bot.user.setActivity(`The New Defenders | !!help`, { type: 'WATCHING' });
+client.on("ready", async () => {
+  console.log(`$client.user.username} is online!`);
+  client.user.setActivity(`The New Defenders | !!help`, { type: 'WATCHING' });
 });
 
-bot.on("message", async message => {
+client.on("message", async message => {
 
   if (message.author.bot) return;
   if (message.channel.type === "dm") return;
 
-  let prefix = '!';
+  let prefix = '!!';
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
@@ -21,4 +21,4 @@ bot.on("message", async message => {
   }
 });
 
-bot.login(process.env.TOKEN);
+client.login(process.env.TOKEN);
