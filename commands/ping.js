a41e1c8a8466dff24745d4763f95c6ea21c6468d
Jackohhh <jackythejack11@gmail.com>
2018-06-21 01:55:51 +1200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, args) => {
 
   message.channel.send("Ping?");
-  edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  msg.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
 
 }
