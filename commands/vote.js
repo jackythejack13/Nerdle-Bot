556fc342fa1e9e041ee766b3803451fbb2ba8d3f
Jackohhh <jackythejack11@gmail.com>
2018-06-21 13:54:47 +1200

    Update vote.js

--- commands/vote.js
+++ commands/vote.js
@@ -1,14 +1,14 @@
 exports.run = aysnc(client, message, args) => {
 
-  let question = args.slice(0).join(" ");
-
-  if (args.length === 0)
-  return message.reply('**Invalid Format:** `!Poll <Question>`')
-  
-  message.channel.send(`<:blobconcil:459168698033504278> **${question}**`)
-  
-  message.react(mesage.guild.emojis.get('422535662470955009'))
-  .then(() => message.react(mesage.guild.emojis.get('422535673984319498>'))
-  .catch(() => console.error('Emoji failed to react.'));
+if (!args) return message.reply("you must have something to vote for!")
+        message.channel.send(`:ballot_box:  ${message.author.username} started a vote! React to my next message to vote on it. :ballot_box: `);
+        const pollTopic = await message.channel.send(message.content.slice(2));
+        await pollTopic.react(`✅`);
+        await pollTopic.react(`⛔`);
+        // Create a reaction collector
+        const filter = (reaction) => reaction.emoji.name === '✅';
+        const collector = pollTopic.createReactionCollector(filter, { time: 15000 });
+        collector.on('collect', r => console.log(`Collected ${r.emoji.name}`));
+        collector.on('end', collected => console.log(`Collected ${collected.size} items`));
 
 }
