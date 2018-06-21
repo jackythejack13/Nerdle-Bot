30b5372b5ad89111c75c0d8a8378d8ab5b571477
Jackohhh <jackythejack11@gmail.com>
2018-06-21 13:19:15 +1200

    Update vote.js

--- commands/vote.js
+++ commands/vote.js
@@ -1,1 +1,20 @@
+exports.run = aysnc(client, message, args) => {
 
+  let question = args.slice(0).join(" ");
+
+  if (args.length === 0)
+  return message.reply('**Invalid Format:** `!Poll <Question>`')
+
+  const embed = new Discord.RichEmbed()
+  .setTitle("A Poll Has Been Started!")
+  .setColor("#5599ff")
+  .setDescription(`${question}`)
+  .setFooter(`Poll Started By: ${message.author.username}`, `${message.author.avatarURL}`)
+
+  message.channel.send({embed})
+  message.react('👍')
+  .then(() => message.react('👎'))
+  .then(() => message.react('🤷'))
+  .catch(() => console.error('Emoji failed to react.'));
+
+}
