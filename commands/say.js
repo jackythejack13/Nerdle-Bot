cedccc09481c05125049725b799c7cf2a82d37c4
Jackohhh <jackythejack11@gmail.com>
2018-06-21 12:07:38 +1200

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,5 +1,6 @@
-exports.run = (client, message, args) => {
+exports.run = async(client, message, args) => {
 	
+	if(!message.member.hasPermission("MANAGAE_MESSAGES")) return message.reply("it doesn't look like you can ue that.");
 	const sayMessage = args.join(' ');
 	message.delete().catch(O_o=>{});
 	message.channel.send(`<:msg:412878967062790144> ${sayMessage}`);
