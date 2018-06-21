20073413eaf0582404ff0d598524d68917c950af
Jackohhh <jackythejack11@gmail.com>
2018-06-21 12:13:03 +1200

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,8 +1,8 @@
 exports.run = async(client, message, args) => {
 	
-	if(!message.member.hasPermission("MANAGAE_MESSAGES")) return message.reply("it doesn't look like you can ue that.");
-	const sayMessage = args.join(' ');
-	message.delete().catch(O_o=>{});
+	if(!message.member.hasPermission("MANAGAE_MESSAGES")) return message.reply("it doesn't look like you can use that.");
+	let sayMessage = args.join(' ');
+	message.delete().catch();
 	message.channel.send(`<:msg:412878967062790144> ${sayMessage}`);
 	
 }
