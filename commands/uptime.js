25ded76b659140f98ceea01038011dd3c13a3718
Jackohhh <jackythejack11@gmail.com>
2018-06-22 11:49:06 +1200

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -1,1 +1,13 @@
+exports.run = async(client, message, args) => {
 
+  let totalSeconds = (client.uptime / 1000);
+  let hours = Math.floor(totalSeconds / 3600);
+  totalSeconds %= 3600;
+  let minutes = Math.floor(totalSeconds / 60);
+  let seconds = totalSeconds % 60;
+  
+  let uptime = `${hours} hours, ${minutes} minutes and ${seconds} seconds`;
+  
+  message.channel.send(uptime);
+  
+}
