40b9e138d51e45eaa0684e42ac1b3715ed0e93d4
Jackohhh <jackythejack11@gmail.com>
2018-06-24 10:11:56 +1200

    Update and rename uptime.js to ascii.js

---
+++ commands/ascii.js
@@ -0,0 +1,16 @@
+const ascii = require('ascii-art');
+
+exports.run = async(client, message, args, ops) => {
+
+  ascii.font(args.join(' '), 'Doom'), function(rendered) {
+      rendered = rendered.trimRight();
+    
+      if (rendered.length > 2000) return message.channel.send(`Sorry, that message is too long, **${message.author.username}**`);
+      
+      message.channel.send(rendered, {
+          code: 'nd'
+      });
+    
+  });
+  
+}
