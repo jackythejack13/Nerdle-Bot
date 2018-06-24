ec71405118ef5821f7db99e07f1808df0298c13a
Jackohhh <jackythejack11@gmail.com>
2018-06-24 13:56:55 +1200

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -1,6 +1,6 @@
 exports.run = async(client, message, args) => {
    
-   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply(`it doesn't look like you can use that. <:blobsmilesweat:460258466687352853>`);
+   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply('it doesn\'t look like you can use that. <:blobsmilesweat:460258466687352853>');
    if(!args[0]) return message.channel.send("oof");
    message.channel.bulkDelete(args[0]).then(() => {
       message.channel.send(`I have deleted **${args[0]}** messages!`).then(msg => msg.delete(5000));
