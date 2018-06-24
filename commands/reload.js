9688387d5814ea045809655921f4e921a45016ab
Jackohhh <jackythejack11@gmail.com>
2018-06-24 12:11:27 +1200

    Update reload.js

--- commands/reload.js
+++ commands/reload.js
@@ -3,7 +3,7 @@ exports.run = (client, message, args, ops) => {
   if (message.author.id !== ops.ownerID) return message.channel.send('Sorry, only **Jackohh#6423** can use this command.');
   
   try {
-    delete require.cache[require.resolve(`./${arg[0]}`)];
+      delete require.cache[require.resolve(`./${arg[0]}.js`)];
   } catch (e) {
       return message.channel.send(`Unable to reload: ${args[0]}`);
   }
