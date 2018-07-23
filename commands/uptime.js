7fc09bba2ba67c06f0b3258b7a70280ad8e120ff
Jackohhh <jackythejack11@gmail.com>
2018-07-23 12:18:59 +1200

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -1,1 +1,10 @@
+const Discord = require('discord.js');
 
+exports.run = async(client, message, args) => {
+  
+  const embed = new Discord.MessageEmbed()
+  .setColor(0x1A7939)
+  .addField('Uptime', `${client.ping / 1000} seconds`)
+  message.channel.send({ embed });
+  
+}
