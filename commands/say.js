aa33b1ee9a2c2ad08415fba9569ee12a1c35661f
Jackohhh <jackythejack11@gmail.com>
2018-07-23 12:30:39 +1200

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,7 +1,7 @@
 exports.run = async(client, message, args) => {
 	
-	if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("it doesn't look like you can use that.");
+	if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("<:tickno:41287883179631796355093> **Access Denied**");
 	const sayMessage = args.join(' ');
 	message.delete().catch();
-	message.channel.send(`<:msg:412878967062790144> ${sayMessage}`);
+	message.channel.send(`${sayMessage}`);
 }
