69b48d84d7b04e523091f1a73c4010b53de37ee5
Jackohhh <jackythejack11@gmail.com>
2018-07-25 11:29:57 +1200

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -1,9 +1,9 @@
 exports.run = async(client, message, args) => {
    
-   if(!message.member.hasPermission("MANAGE_MESSAGES") || message.author.id !== ops.ownerID) return message.channel.send(`${message.author.username}, Sorry, it doesn't look like you can use that. ðŸ˜…`);
-   if(!args[0]) return message.channel.send("Please specify the number of messages to clear, **${message.author.username}**.");
+   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(`${message.author.username}, it doesn't look like you can use that. :sweat_smile:`);
+   if(!args[0]) return message.channel.send(`Please specify the number of messages to clear, ${message.author.username}.`);
       message.delete()
 	  message.channel.bulkDelete(args[0]).then(() => {
-      message.channel.send(`I have deleted \`${args[0]} messages\` for you! ðŸ™‚`).then(msg => msg.delete(5000));
+      message.channel.send(`I have deleted \`${args[0]} messages\` for you! :slight_smile:`).then(msg => msg.delete(5000));
    });
 }
