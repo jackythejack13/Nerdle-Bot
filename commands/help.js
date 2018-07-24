e1a989790a090b5cb0b7ab2f9b0f0fe0265ebce4
Jackohhh <jackythejack11@gmail.com>
2018-07-25 11:30:12 +1200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,4 +3,3 @@ exports.run = (client, message, args) => {
 	message.channel.send('**Commands**\n\n`help` - Shows this message \n`ping` - Checks the response time \n`say <message>` - Make the bot say something \n`8ball <question>` - Let 8ball deicide your fate \n`clear <count>` - Clear messages \n`invite` - Get the server invite \n`uptime` - Get bot uptime \n`reload <cmd>` - Reloads a command \n`ascii <msg>` - Takes a text and makes it into ASCII art \n')
 	
 }
-
