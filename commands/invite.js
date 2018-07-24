d1c3a8ee616acb0c0632dafac13184f70f5f3731
Jackohhh <jackythejack11@gmail.com>
2018-07-25 11:30:35 +1200

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,5 +1,5 @@
 exports.run = (client, message, args) => {
 
-  message.channel.send(':linkgreen: **Permanent Invite Link:** <https://discord.gg/ujqmgae>');
+  message.channel.send('<:linkgreen:465668520743796736> **Permanent Invite Link:** <https://discord.gg/ujqmgae>');
 
 }
