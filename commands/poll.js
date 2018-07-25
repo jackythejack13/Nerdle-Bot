ed12711c5b4abf2e19e3455c1905842e00fdba69
Jackohhh <jackythejack11@gmail.com>
2018-07-26 02:15:48 +1200

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -1,12 +1,12 @@
 const Discord = require('discord.js');
 
-exports.run = async(client, message, args, tools) => {
+exports.run = async(client, message, args) => {
 
   if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send(`${message.author.username}, it doesn't look like you can use that.`);
   
   if (!args[0]) return message.channel.send('**Usage**: !!poll <question>');
   
-  const embed = new Discord.MessageEmbed()
+  const embed = new Discord.RichEmbed()
   .setcolor()
   .setFooter(`From ${message.author.username}#${message.author.discriminator}`)
   .setDescription(`**${args.join(' ')}**`)
