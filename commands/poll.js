fc50237e281905d5543119f5c27ec0f636003b7a
Jackohhh <jackythejack11@gmail.com>
2018-07-26 01:58:47 +1200

    Create poll.js

---
+++ commands/poll.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+
+exports.run = async(client, message, args, tools) => {
+
+  if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send(`${message.author.username}, it doesn't look like you can use that.`);
+  
+  if (!args[0]) return message.channel.send('**Usage**: !!poll <question>');
+  
+  const embed = new Discord.MessageEmbed()
+  .setcolor()
+  .setFooter(`From ${message.author.username}#${message.author.discriminator}`)
+  .setDescription(`**${args.join(' ')}**`)
+  .setTitle('Poll ');
+  
+  let msg = message.channel.send(embed);
+  
+  await msg.react('👍');
+  await msg.react('👎');
+  await msg.react('🤷');
+  
+  message.delete({timeout: 1000})
+
+}
